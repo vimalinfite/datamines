ddf9a779dbc94c5196bd31a238808e83b17146f0
vimalinfite <154795948+vimalinfite@users.noreply.github.com>
2024-04-20 00:53:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 3000
-  }
+ server: {
+    port: 5173,
+  },
 })
